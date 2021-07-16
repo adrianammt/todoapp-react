ebb1d0741292b018a2a47d7458714c4f7a06c3ac
adrianammt <amorat@gmx.de>
2021-07-16 16:03:06 +0200

    Create the Form and List Item components, the Items are now displaying

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import "./App.css";
-
-/*<header>
-  <form>
-    <input></input>
-    <button></button>
-  <form/>
-
-  TodoElements =
-  {
-    isTodoDeleted: false(default)
-    name:TodoName
-    todoStatus: pending(default)
-  }
-*/
+import TodoForm from "./components/TodoForm";
+import { useState } from "react";
 
 function App() {
+  const [todoListItems, setTodoListItems] = useState([]);
+  console.log(todoListItems);
+  function handleSubmit(event) {
+    console.log("works");
+
+    event.preventDefault();
+    const form = event.target;
+    const input = form.todoItemInput;
+    const newTodo = input.value;
+    if (newTodo) {
+      const newTodoItems = [
+        ...todoListItems,
+        {
+          name: newTodo,
+          id: Math.floor(Math.random() * 1000),
+          isComplete: false,
+        },
+      ];
+      console.log("Hola");
+      setTodoListItems(newTodoItems);
+    } else {
+      alert("Please type in a TODO!");
+    }
+    form.reset();
+  }
+
+  function TodoListItems({ showTodos, todo, status }) {
+    return (
+      <li className="List-Element">
+        <p>{todo}</p>
+        <button className="Element-Status">Pending</button>
+        <button className="Delete-Element">X</button>
+      </li>
+    );
+  }
+  const showTodos = todoListItems.map((newTodo, index) => {
+    console.log(newTodo);
+    console.log(newTodo.name);
+    return (
+      <TodoListItems
+        key={index}
+        todo={newTodo.name}
+        status={newTodo.isComplete}
+      />
+    );
+  });
+
+  //==================App Begin
   return (
     <div className="App">
       <header className="App-Header">
-        <form className="App-Header_Form">
-          <input
-            type="text"
-            id="AppInput"
-            name="AppInput"
-            placeholder="Type in your TODO"
-          ></input>
-          <button className="AddTodoClick">Add</button>
-        </form>
+        <TodoForm addTodo={handleSubmit} />
       </header>
-      <main className="App-Content">
-        <ul className="ListHolder">
-          <li className="List-Element">
-            <button className="Delete-Element">X</button>
-            <p>Do my App</p>
-            <button className="Element-Status">Pending</button>
-          </li>
-        </ul>
-      </main>
-      <footer className="App-Footer">
-        <p className="App-Footer_Quote">Made with 💙 by Adriana</p>
-      </footer>
+      <main className="App-Content">{showTodos}</main>
     </div>
   );
 }
+//==================App End
 
 export default App;
